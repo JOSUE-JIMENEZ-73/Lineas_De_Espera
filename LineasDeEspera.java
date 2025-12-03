1e31571f66484945cb1313b998a98c9151cd3af3
JOSUE-JIMENEZ-73 <jimenezlaptop08@gmail.com>
2025-12-02 20:16:31 -0600

    Agregar funcionalidad para calcular el modelo M/M/1 y mejorar la estructura del menú

--- LineasDeEspera.java
+++ LineasDeEspera.java
@@ -11,47 +11,91 @@ import java.util.Scanner;
 
 public class LineasDeEspera {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);    
-        int opcion=0;
+        Scanner sc = new Scanner(System.in);
+        int opcion = 0;
         do {
-            System.out.println("Seleccione el modelo de línea de espera que desea calcular:");
+            // Menú de opciones
+            System.out.println("\n===============MNEU===============");
+            System.out.println("Seleccione el modelo de línea de\nespera que desea calcular:");
             System.out.println("1. Modelo M/M/1");
             System.out.println("2. Modelo M/M/C");
             System.out.println("3. Modelo M/M/1/K");
             System.out.println("4. Salir");
+            System.out.print("Ingrese su opción: ");
             opcion = sc.nextInt();
 
             switch (opcion) {
-                case 1:{
-
+                case 1: {
+                    System.out.println("\n===============Modelo M/M/1===============");
+                    System.out.println("Ingrese el promedio de llegadas: ");
+                    int promedioLlegadas = sc.nextInt();
+                    System.out.println("Ingrese el promedio de servicio: ");
+                    int promdedioServicio = sc.nextInt();
+                    modeloMM1(promedioLlegadas, promdedioServicio);
+                    break;
                 }
+                case 2: {
+                    modeloMMC();
                     break;
-                case 2:
+                }
+                case 3: {
+                    modeloMM1K();
                     break;
-                case 3:
-                    break;
-                case 4:
+                }
+                case 4: {
                     System.out.println("Saliendo del programa...");
                     break;
+                }
                 default:
                     System.out.println("Opción inválida. Por favor, seleccione una opción válida.");
             }
-        } while (opcion!=4);
+        } while (opcion != 4);
+        sc.close();
     }
 
-    //Metodos para los calculos
-    //Modelo M/M/1
-    public static void modeloMM1() {
-        
+    // Metodos para los calculos
+    // Modelo M/M/1
+    // λ = promedio de llegadas
+    // μ = promedio de servicio
+    public static void modeloMM1(int lambda, int mu) {
+        if (lambda < mu) {
+            
+            double p= (double) lambda / mu; // factor de utilizacion del sistema
+            p = (int) (p * 100.0) / 100.0;
+
+            double Lq=(Math.pow(lambda,2))/(mu*(mu-lambda) ) ;// numero promedio de clientes en la cola
+            Lq = (int) (Lq * 100.0) / 100.0;
+
+            double Wq= (double) Lq/lambda ; // tiempo promedio de espera en la cola
+            Wq = (int) (Wq * 100.0) / 100.0;
+
+            double W= (double) Wq + (1/mu); // tiempo total en el sistema
+            W = (int) (W * 100.0) / 100.0;
+
+            double L=(double) lambda*W ;//numero promedio de clientes en el sistema
+            L = (int) (L * 100.0) / 100.0;
+                    
+
+            System.out.println("\nEl sistema es estable.");
+            System.out.println("Factor de utilizacion del sistema: "+ p);
+            System.out.println("Número promedio de clientes en la cola: "+ Lq);
+            System.out.println("Tiempo promedio de espera en la cola: "+ Wq +" horas o "+ (Wq*60)+" minutos.");
+            System.out.println("Tiempo total en el sistema: "+ W +" horas o "+ (W*60)+" minutos.");
+            System.out.println("Número promedio de clientes en el sistema: "+ L);
+        } else {
+            System.out.println("\nEl sistema no es estable, ya que el promedio de\nllegadas debe ser menor que el promedio de servicio.");
+        }
+
     }
-    //Modelo M/M/C
+
+    // Modelo M/M/C
     public static void modeloMMC() {
-        
+
     }
-    //Modelo M/M/1K
+
+    // Modelo M/M/1K
     public static void modeloMM1K() {
-        
+
     }
-    
 
 }
