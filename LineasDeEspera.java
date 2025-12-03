1743fa988650a6af1c14ac3c018f64c4f747bbe9
JOSUE-JIMENEZ-73 <jimenezlaptop08@gmail.com>
2025-12-02 19:40:05 -0600

    Agregar menú interactivo para seleccionar modelos de línea de espera

--- LineasDeEspera.java
+++ LineasDeEspera.java
@@ -6,12 +6,25 @@ C = numero de servidores
 a = intensidad de trafico 
 ρ = factor de utilizacion del sistema
 */
+
+import java.util.Scanner;
+
 public class LineasDeEspera {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);    
         int opcion=0;
         do {
+            System.out.println("Seleccione el modelo de línea de espera que desea calcular:");
+            System.out.println("1. Modelo M/M/1");
+            System.out.println("2. Modelo M/M/C");
+            System.out.println("3. Modelo M/M/1/K");
+            System.out.println("4. Salir");
+            opcion = sc.nextInt();
+
             switch (opcion) {
-                case 1:
+                case 1:{
+
+                }
                     break;
                 case 2:
                     break;
@@ -25,4 +38,20 @@ public class LineasDeEspera {
             }
         } while (opcion!=4);
     }
+
+    //Metodos para los calculos
+    //Modelo M/M/1
+    public static void modeloMM1() {
+        
+    }
+    //Modelo M/M/C
+    public static void modeloMMC() {
+        
+    }
+    //Modelo M/M/1K
+    public static void modeloMM1K() {
+        
+    }
+    
+
 }
