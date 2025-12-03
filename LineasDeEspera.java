187419ac511f080f7a7c3b488f7d9638305a6c3f
JOSUE-JIMENEZ-73 <jimenezlaptop08@gmail.com>
2025-12-02 19:35:08 -0600

    Agregar clase LineasDeEspera con comentarios sobre variables

---
+++ LineasDeEspera.java
@@ -0,0 +1,14 @@
+/*
+Variables:
+λ =promdedio de llegadas
+μ =promedio de servicio
+C = numero de servidores
+a = intensidad de trafico 
+ρ = factor de utilizacion del sistema
+*/
+public class LineasDeEspera {
+    public static void main(String[] args) {
+        
+        
+    }
+}
